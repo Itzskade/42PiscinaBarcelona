952c42df983d871b9329815c49fad21c4538aa66
Itzskade <itzskade@gmail.com>
2025-07-14 11:00:01 +0200

    Add files via upload

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarin-n <rmarin-n@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 10:54:40 by rmarin-n          #+#    #+#             */
+/*   Updated: 2025/07/14 10:55:33 by rmarin-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
+
+/*
+int	main()
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
+*/
