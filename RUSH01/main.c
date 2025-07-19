69fdd3bf1fcf2171f1e160c88824a1fc172bf52f
Itzskade <itzskade@gmail.com>
2025-07-19 21:55:19 +0200

    Update main.c

--- RUSH01/main.c
+++ RUSH01/main.c
@@ -1,16 +1,48 @@
 /*Entrada del programa*/
 
+#include <stdlib.h>
 #include "utils.h"
+#include "input.h"
 
 int	main(int argc, char **argv)
-{	
+{
 	int	clue[16];
-	int	grid[4][4];	
+	int	**grid;
+	int	i;
 
-	if (argc != 2)
+	if (argc != 2 || !parse_input(argv[1], clue))
 	{
-		write(1, "Error\n", 6);
-		return (1);
+		write_error();
+		return 1;
 	}
-	return (0);
+	grid = malloc(4 * sizeof(int *));
+	if (!grid)
+	{
+		write_error();
+		return 1;
+	}
+	i = 0;
+	while (i < 4)
+	{
+		grid[i] = malloc(4 * sizeof(int));
+		if (!grid[i])
+		{
+			write_error();
+			return 1;
+		}
+		i++;
+	}
+	init_grid(grid);
+	if (solve(grid, 0, 0, clue))
+		print_grid(grid);
+	else
+		write_error();
+	i = 0;
+	while (i < 4)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+	return 0;
 }
