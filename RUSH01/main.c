1a52e1f82019edc127c2c85e69adb7f8127f126f
Itzskade <itzskade@gmail.com>
2025-07-19 22:16:46 +0200

    Update main.c

--- RUSH01/main.c
+++ RUSH01/main.c
@@ -1,8 +1,32 @@
-/*Entrada del programa*/
-
 #include <stdlib.h>
-#include "utils.h"
-#include "input.h"
+#include "skyscraper.h"
+
+int	parse_input(char *str, int *clues)
+{
+	int	i;
+	int	value;
+
+	i = 0;
+	while (*str)
+	{
+		while (*str == ' ')
+			str++;
+		if (*str >= '1' && *str <= '4')
+		{
+			value = *str - '0';
+			clues[i] = value;
+			i++;
+			str++;
+		}
+		else if (*str != '\0')
+		{
+			return 0;
+		}
+	}
+	if (i != 16)
+		return 0;
+	return 1;
+}
 
 int	main(int argc, char **argv)
 {
