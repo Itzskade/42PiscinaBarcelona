8388d24df90804a5ed07c33fff08a4e69690fd23
Itzskade <itzskade@gmail.com>
2025-07-20 10:48:20 +0200

    Actualizar solver.c

--- RUSH01/ex00/solver.c
+++ RUSH01/ex00/solver.c
@@ -21,6 +21,24 @@ int count_visible(int *line)
     return (visible);
 }
 
+int 	validate_line(int *line, int clue)
+{
+    int 	i;
+    int 	count;
+
+    i = 0;
+    while (i < 4)
+    {
+        if (line[i] == 0)
+            return (1);
+        i++;
+    }
+    if (clue == 0)	
+        return (1);
+    count = count_visible(line);
+    return (count == clue);
+}
+
 int is_valid_grid(int **grid, int *clues)
 {
     int row;
