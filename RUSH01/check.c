c09d0467fbd1a2071afb8df97125e58f1aae442a
Itzskade <itzskade@gmail.com>
2025-07-20 02:32:29 +0200

    Update check.c

--- RUSH01/check.c
+++ RUSH01/check.c
@@ -9,13 +9,16 @@ Right		    12 to 15
 #include "skyscraper.h"
 #include <unistd.h>
 
-int count_visible(int *line);
+int    count_visible(int *line);
+int    validate_line(int *line, int clue);
 
-int check_row_left(int **grid, int *clues)
+int    check_row_left(int **grid, int *clues)
 {
-    int row = 0, col, visible;
+    int row;
+    int col;
     int line[4];
 
+    row = 0;
     while (row < 4)
     {
         col = 0;
@@ -24,22 +27,20 @@ int check_row_left(int **grid, int *clues)
             line[col] = grid[row][col];
             col++;
         }
-        visible = count_visible(line);
-        if (visible != clues[8 + row])
-        {
-            write_error();
-            return 0;
-        }
+        if (!validate_line(line, clues[8 + row]))
+            return (0);
         row++;
     }
-    return 1;
+    return (1);
 }
 
 int check_row_right(int **grid, int *clues)
 {
-    int row = 0, col, visible;
+    int row;
+    int col;
     int line[4];
 
+    row = 0;
     while (row < 4)
     {
         col = 0;
@@ -48,22 +49,20 @@ int check_row_right(int **grid, int *clues)
             line[col] = grid[row][3 - col];
             col++;
         }
-        visible = count_visible(line);
-        if (visible != clues[12 + row])
-        {
-            write_error();
-            return 0;
-        }
+        if (!validate_line(line, clues[12 + row]))
+            return (0);
         row++;
     }
-    return 1;
+    return (1);
 }
 
 int check_col_up(int **grid, int *clues)
 {
-    int col = 0, row, visible;
+    int col;
+    int row;
     int line[4];
 
+    col = 0;
     while (col < 4)
     {
         row = 0;
@@ -72,22 +71,20 @@ int check_col_up(int **grid, int *clues)
             line[row] = grid[row][col];
             row++;
         }
-        visible = count_visible(line);
-        if (visible != clues[col])
-        {
-            write_error();
-            return 0;
-        }
+        if (!validate_line(line, clues[col]))
+            return (0);
         col++;
     }
-    return 1;
+    return (1);
 }
 
-int check_col_down(int **grid, int *clues)
+int    check_col_down(int **grid, int *clues)
 {
-    int col = 0, row, visible;
+    int col;
+    int row;
     int line[4];
 
+    col = 0;
     while (col < 4)
     {
         row = 0;
@@ -96,13 +93,22 @@ int check_col_down(int **grid, int *clues)
             line[row] = grid[3 - row][col];
             row++;
         }
-        visible = count_visible(line);
-        if (visible != clues[4 + col])
-        {
-            write_error();
-            return 0;
-        }
+        if (!validate_line(line, clues[4 + col]))
+            return (0);
         col++;
     }
-    return 1;
+    return (1);
+}
+
+int    check_views(int **grid, int *clues)
+{
+    if (!check_row_left(grid, clues))
+        return (0);
+    if (!check_row_right(grid, clues))
+        return (0);
+    if (!check_col_up(grid, clues))
+        return (0);
+    if (!check_col_down(grid, clues))
+        return (0);
+    return (1);
 }
