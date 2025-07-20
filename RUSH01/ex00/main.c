9378e1757aba4817390c86121a82478336d9a42e
Itzskade <itzskade@gmail.com>
2025-07-20 04:17:39 +0200

    Add files via upload

---
+++ RUSH01/ex00/main.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include "skyscraper.h"
+
+int     read_clues(char *str, int *clues)
+{
+    int     i;
+    int     value;
+
+    i = 0;
+    while (*str)
+    {
+        while (*str == ' ')
+            str++;
+        if (*str >= '1' && *str <= '4')
+        {
+            value = *str - '0';
+            clues[i++] = value;
+            str++;
+        }
+        else if (*str != '\0')
+            return (0);
+    }
+    if (i != 16)
+        return (0);
+    return (1);
+}
+
+int     main(int argc, char **argv)
+{
+    int     clue[16];
+    int     **grid;
+
+    if (argc != 2 || !read_clues(argv[1], clue))
+        return (write_error(), 1);
+    if (!init_grid(&grid))
+        return (1);
+    if (solve(grid, 0, 0, clue))
+        print_grid(grid);
+    else
+        write_error();
+    free_grid(grid, 4);
+    return (0);
+}
