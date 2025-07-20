29486d1e5204658861b8bffb03d42d02abc6aebc
Itzskade <itzskade@gmail.com>
2025-07-20 10:47:53 +0200

    Actualizar utils.c

--- RUSH01/ex00/utils.c
+++ RUSH01/ex00/utils.c
@@ -6,24 +6,6 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int 	validate_line(int *line, int clue)
-{
-    int 	i;
-    int 	count;
-
-    i = 0;
-    while (i < 4)
-    {
-        if (line[i] == 0)
-            return (1);
-        i++;
-    }
-    if (clue == 0)	
-        return (1);
-    count = count_visible(line);
-    return (count == clue);
-}
-
 void	write_error(void)
 {
 	write(1, "Error\n", 6);
