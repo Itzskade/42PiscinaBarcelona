14698e249ef16aea902fcf5c9068822d7a5efd3b
Itzskade <itzskade@gmail.com>
2025-07-20 04:16:54 +0200

    Add files via upload

---
+++ RUSH01/solver.c
@@ -0,0 +1,79 @@
+#include "skyscraper.h"
+
+int count_visible(int *line)
+{
+    int i;
+    int max_height;
+    int visible;
+
+    i = 0;
+    max_height = 0;
+    visible = 0;
+    while (i < 4)
+    {
+        if (line[i] > max_height)
+        {
+            max_height = line[i];
+            visible++;
+        }
+        i++;
+    }
+    return (visible);
+}
+
+int is_valid_grid(int **grid, int *clues)
+{
+    int row;
+    int col;
+
+    row = 0;
+    while (row < 4)
+    {
+        col = 0;
+        while (col < 4)
+        {
+            if (grid[row][col] == 0)
+                return (0);
+            col++;
+        }
+        row++;
+    }
+    return (check_views(grid, clues));
+}
+
+int is_safe(int **grid, int row, int col, int num)
+{
+    int i;
+
+    i = 0;
+    while (i < 4)
+    {
+        if (grid[row][i] == num || grid[i][col] == num)
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+int solve(int **grid, int row, int col, int *clues)
+{
+    int num;
+
+    if (row == 4)
+        return (is_valid_grid(grid, clues));
+    if (col == 4)
+        return (solve(grid, row + 1, 0, clues));
+    num = 1;
+    while (num <= 4)
+    {
+        if (is_safe(grid, row, col, num))
+        {
+            grid[row][col] = num;
+            if (solve(grid, row, col + 1, clues))
+                return (1);
+            grid[row][col] = 0;
+        }
+        num++;
+    }
+    return (0);
+}
