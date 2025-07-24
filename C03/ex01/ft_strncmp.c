84dd701eb7c2c91311ede301941c511f49af5577
Skadito <itzskade@gmail.com>
2025-07-24 19:16:41 +0200

    Update ft_strncmp.c

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -15,11 +15,17 @@
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	while ((*s1 < n) && (*s1 && *s2) && (*s1 == *s2))
+	unsigned int	i;
+
+	i = 0;
+	while ((i < n) && (*s1 && *s2) && (*s1 == *s2))
 	{
 		s1++;
-		s2++; 	
+		s2++;
+		i++;
 	}
+	if (i == n)
+		return (0);
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
 /*
