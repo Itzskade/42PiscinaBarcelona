73799d9ae94bddd7ebdb15c2bbb43701f5728651
Skadito <itzskade@gmail.com>
2025-07-27 11:54:20 +0200

    Actualizar utils2.c

--- RUSH02/utils2.c
+++ RUSH02/utils2.c
@@ -32,6 +32,36 @@ void	ft_bzero(void *s, int n)
 	}
 }
 
+#include "libft.h"
+#include <stdlib.h>
+
+char	*pad_triplet(char *triplet)
+{
+	int		len;
+	char	*padded;
+
+	len = ft_strlen(triplet);
+	padded = malloc(4);
+	if (!padded)
+		return (NULL);
+	padded[3] = '\0';
+	if (len == 3)
+		ft_strncpy(padded, triplet, 3);
+	else if (len == 2)
+	{
+		padded[0] = '0';
+		padded[1] = triplet[0];
+		padded[2] = triplet[1];
+	}
+	else if (len == 1)
+	{
+		padded[0] = '0';
+		padded[1] = '0';
+		padded[2] = triplet[0];
+	}
+	return (padded);
+}
+
 int	is_zero_triplet(char *triplet)
 {
 	int	i;
