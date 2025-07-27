b80882ca6467fe5ab2a760c6f3507d040c5f2f5e
Skadito <itzskade@gmail.com>
2025-07-27 09:39:19 +0200

    Crear read_file.c

---
+++ RUSH02/read_file.c
@@ -0,0 +1,28 @@
+read_file.c
+#include "structs.h"
+#include <fcntl.h>
+
+char	*read_file(char *filename)
+{
+	int		fd;
+	int		bytes;
+	char	buffer[4096];
+	char	*content;
+	int		i;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	bytes = read(fd, buffer, 4096);
+	close(fd);
+	if (bytes <= 0)
+		return (0);
+	content = malloc(bytes + 1);
+	if (!content)
+		return (0);
+	i = -1;
+	while (++i < bytes)
+		content[i] = buffer[i];
+	content[i] = '\0';
+	return (content);
+}
