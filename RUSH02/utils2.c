5e002de1fba7ec668aadaa0003b569a5b8927a08
Skadito <itzskade@gmail.com>
2025-07-27 10:09:33 +0200

    Actualizar utils2.c

--- RUSH02/utils2.c
+++ RUSH02/utils2.c
@@ -1,1 +1,33 @@
-r
+#include "structs.h"
+
+char	*ft_strncpy(char *dest, char *src, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+void	ft_bzero(void *s, int n)
+{
+	int		i;
+	char	*ptr;
+
+	ptr = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+}
