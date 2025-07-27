f6c0208687c37c27b5eab9e3c88d0e857426ec94
Skadito <itzskade@gmail.com>
2025-07-27 11:50:37 +0200

    Actualizar process_number.c

--- RUSH02/process_number.c
+++ RUSH02/process_number.c
@@ -23,3 +23,29 @@ void	print_scale_name(t_dict *dict, int size, int index)
 	free(scale_key);
 	print_value(value);
 }
+
+void	process_number(t_dict *dict, int size, char *number)
+{
+	int		len;
+	int		triplet_count;
+	int		i;
+	int		start;
+	char	*triplet;
+
+	len = ft_strlen(number);
+	triplet_count = (len + 2) / 3;
+	i = -1;
+	while (++i < triplet_count)
+	{
+		start = len - 3 * (i + 1);
+		if (start < 0)
+			start = 0;
+		triplet = ft_substr(number, start, len - 3 * i - start);
+		if (!is_zero_triplet(triplet))
+		{
+			process_triplet(dict, size, pad_triplet(triplet));
+			print_scale_name(dict, size, triplet_count - i - 1);
+		}
+		free(triplet);
+	}
+}
