39859d734422e6e757a3e7a5106356484beca2f7
Skadito <itzskade@gmail.com>
2025-07-27 10:06:35 +0200

    Actualizar utils.c

--- RUSH02/utils.c
+++ RUSH02/utils.c
@@ -1,1 +1,81 @@
-h
+#include "structs.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dup;
+	int		len;
+	int		i;
+
+	len = 0;
+	while (src[len])
+		len++;
+	dup = malloc(len + 1);
+	if (!dup)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_substr(char *str, int start, int len)
+{
+	char	*sub;
+	int		i;
+
+	if (!str)
+		return (0);
+	sub = malloc(len + 1);
+	if (!sub)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = str[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+int	is_valid_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	if (str[0] == '\0')
+		return (0);
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
